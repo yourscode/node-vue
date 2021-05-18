4d2ed76bdb7ceab204e547bbfb51f8e9de08ef6f
huzhihui <huzhihui.com>
2021-05-18 10:30:18 +0800

    添加输出客户经理报表

--- testnode.js
+++ testnode.js
@@ -203,6 +203,79 @@ app.get('/exportExcel2', function(req, res) {
     }
   })
 })
+// 后台接口输出客户汇总表
+app.get('/exportExcel3', function(req, res) {
+  // 创建一个写入格式map，其中cols(表头)，rows(每一行的数据);
+  const conf = {}
+  // 手动创建表头中的内容
+  // 在conf中添加cols
+  conf.cols = []
+  pool.getConnection((err, conn) => {
+    if (err) {
+      // 执行出错
+      throw err
+    } else {
+      var sql = `SELECT f.* ,(f.通话用户数/f.目标用户数) as 通话率,(f.走访用户数/f.目标用户数) as 走访率 from  (select COALESCE(d.看管人姓名,'总计') AS 看管人姓名,d.区县编号,count(看管人姓名) 目标用户数,SUM(d.是否通话) 通话用户数,SUM(d.是否走访) 走访用户数  from
+      (select a.* ,case when c.phoneNumber  is not null then '是' else '否' end 是否走访  from 
+      (select b.phone_id,a.* , case when a.地市 is not null then '是' else '否' end 是否通话 
+       from sheet1 a INNER JOIN converse b on a.集团成员手机号 = b.phone_id) a LEFT JOIN tel c on a.phone_id = c.phoneNumber) d GROUP BY 区县编号,看管人姓名  WITH ROLLUP) f ;`
+      conn.query(sql, (err, data) => {
+        if (err) throw err
+        const rows = []
+        const tempArr = ['区县编号', '看管人姓名', `目标用户数`, '通话用户数', '走访用户数', '通话率', '走访率']
+        // console.log(data)
+        for (const key in data[0]) {
+          if (tempArr.indexOf(key) !== -1) {
+            const tits = {}
+            // 添加内容
+            tits.caption = key
+            // 添加对应类型，这类型对应数据库中的类型，入number，data但一般导出的都是转换为string类型的
+            tits.type = 'string'
+            // 将每一个表头加入cols中
+            conf.cols.push(tits)
+            rows.push(key)
+          }
+        }
+        // console.log(rows)
+        // 用于承载数据库中的数据
+        const datas = []
+        // 循环数据库得到的数据
+        // data = data[0]
+        for (let i = 0; i < data.length; i++) {
+          const row = []// 用来装载每次得到的数据
+          // 内循环取出每个字段的数据
+          for (let j = 0; j < rows.length; j++) {
+            row.push(data[i][rows[j] + ''] + '')
+          }
+          // 将每一个{ }中的数据添加到承载中
+          datas.push(row)
+        }
+        // console.log(datas)
+        conf.rows = datas
+        // 将所有数据写入nodeExcel中
+        const result = nodeExcel.execute(conf)
+        // 设置响应头，在Content-Type中加入编码格式为utf-8即可实现文件内容支持中文
+        res.setHeader('Content-Type', 'application/vnd.openxmlformats;charset=utf-8')
+        // 设置下载文件命名，中文文件名可以通过编码转化写入到header中。
+        res.setHeader('Content-Disposition', 'attachment; filename=' + encodeURI('客户经理信息汇总表') + '.xlsx')
+        // 将文件内容传入
+        res.end(result, 'binary')
+      })
+      conn.release()
+      //! !用于结束后清空文件夹
+      // var files = fs.readdirSync(folderName)
+      // files.forEach((file) => {
+      //   if (file === '通话.xlsx') {
+      //     fs.unlink(folderName + '\\' + file, (err) => {
+      //       if (err) throw err
+      //       console.log('通话.xlsx已删除...')
+      //     })
+      //   }
+      // })
+    }
+  })
+})
+
 function insert(sql, item) {
   return new Promise((resolve, reject) => {
     pool.getConnection((_err, conn) => {
@@ -359,6 +432,7 @@ function modifyFolder(params) {
           // console.log('浏览器已经打开！！！！！')
           chrome.exec('start http://localhost:3000/exportExcel')
           chrome.exec('start http://localhost:3000/exportExcel2')
+          chrome.exec('start http://localhost:3000/exportExcel3')
         })
       } else {
         console.log('请放入：走访.xlsx、通话.xlsx两个excel文件！')
